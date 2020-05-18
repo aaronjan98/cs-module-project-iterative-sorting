e238d1dae7823da553247c3bd8c833182be46191
aaronjan <aaronjan98@gmail.com>
2020-05-18 14:43:15 -0700

    Update searching.py
    completed binary_search fxn

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,4 @@
 def linear_search(arr, target):
-    print(arr)
     for i in range(len(arr)):
         if arr[i] is target:
             return i
@@ -8,8 +7,29 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    # let's figure out the total size of the arr 
+    left = 0
+    right = len(arr) - 1
+    
+    while left <= right:
+        # find the midpoint 
+        mid = (left + right) // 2
 
-    # Your code here
+        # check to see if the midpoint element is our target
+        if arr[mid] == target:
+            return mid 
+        
+        # check to see if the element should be on the left or right side 
+        # of our midpoint 
+        if arr[mid] < target:
+            # toss out the left side of the arr 
+            # update our `left` index
+            left = mid + 1
 
-
-    return -1  # not found
+        # otherwise, arr[mid] > target 
+        else:
+            # toss out the right side of the arr because the element has to be on the left side 
+            right = mid - 1
+    
+    # we didn't find the element 
+    return -1
